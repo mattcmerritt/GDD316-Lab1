5572355cd704f4d1f60d6da3b2aa8075d992072e
mattcmerritt <mattcmerritt@gmail.com>
2023-02-10 03:05:50 -0500

    added simple propellor to spin boid

--- Assets/Scripts/PropellorSpin.cs
+++ Assets/Scripts/PropellorSpin.cs
@@ -2,17 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Spinning the plane's propellor relative to the local position of the plane
+
 public class PropellorSpin : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject Propellor;
+    [SerializeField] private Quaternion TargetRotation;
+    [SerializeField] private float TargetAngle;
+    [SerializeField, Range(0, 1000)] private float RotationRate;
+
+    // default the rotation values
+    private void Start()
     {
-        
+        TargetAngle = 0f;
+        TargetRotation = Quaternion.AngleAxis(TargetAngle, Vector3.forward);
+        Propellor.transform.localRotation = TargetRotation;
     }
 
-    // Update is called once per frame
-    void Update()
+    // move the propellor on every physics update
+    private void FixedUpdate()
     {
-        
+        TargetAngle += RotationRate * Time.fixedDeltaTime;
+        TargetRotation = Quaternion.AngleAxis(TargetAngle, Vector3.forward);
+        Propellor.transform.localRotation = TargetRotation;
     }
 }
